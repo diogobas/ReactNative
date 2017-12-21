a900a42dff5056ce28b1c87e125053bb83ecdfac
Diogo Bastos <diogo.bastos@neovation.com>
2017-12-21 15:35:38 -0600

    Adding TODOs code

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Playground from './components/playground/Playground';
@@ -7,12 +8,14 @@ import Scroll from './components/playground/Scroll';
 import FlatListBasics from './components/playground/FlatListBasics';
 import SectionListBasics from './components/playground/SectionListBasics';
 import Movies from './components/playground/Movies';
+import ListView from './components/todo/ListView';
+
 
 export default class App extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <PizzaTranslator />
+                <ListView></ListView>
             </View>
         );
   }
@@ -21,8 +24,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    paddingTop: 50,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    paddingRight: 2,
+    backgroundColor: '#F8F8F8',
+  }
 });
